7e82f43ac56d737af0dcad029d42f3e8269454e4
ShanWehttam <mnash@hotmail.co.uk>
2020-06-18 13:39:12 +0100

    Update general_tips.js

--- general_tips.js
+++ general_tips.js
@@ -1,1 +1,16 @@
+//Destructuring an array or a string gives an index of each element
+  {...[1,2,3]} {..."1,2,3"}
+//parsefloat instead of parseInt because they're both stricter than Number.  Number() allows strings
+  parseInt("564367457868423.00")
+//break and continue can only be used within for loop statements.  I shouldn't need these when i have .filter
+//return stops the function there and then
+//paralell assignment to an expression that returns an array
+  [one, two] = [<array of some sort>]
+//An iterable is a type of Object that can have a 'for of' loop used on it.  Strings and Arrays are Iterables.  Not Objects
+//Strict mode stops us from declaring a variable without const or let
+  "use strict"
+
+
+
+
 

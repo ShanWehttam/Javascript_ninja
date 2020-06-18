c5ceb5533ad92a3dda38ed85128c0c6631ec5eff
ShanWehttam <mnash@hotmail.co.uk>
2020-06-18 13:11:42 +0100

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -27,7 +27,7 @@ let siblings = {siblings: ["brad","holly","James"]}
   //Using computed values to change an array of arrays to an array of hashes
     [[1,2],[3,4]].map( ([key,val]) => ({[key]: val}) )
   //Destructuring properties from an array of hashes
-    [{me: "matthew"}, {you: "michelle"}].map(({me,you}) => me || you )
+    [{me: "matthew"}, {you: "michelle"}].map(({me,you}, ind) => me || you )
   //Pretify an object or the returning JSON
     JSON.stringify(famo, ['dad', 'mum'], "\t")
 
